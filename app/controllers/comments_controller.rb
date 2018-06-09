0084eb271ac7c2cf9a1f32edbe6cf8ff768bdf7f
github username <github email address>
2018-06-09 10:39:57 -0400

    built out Comment model, controller, migration

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,45 @@
 class CommentsController < ApplicationController
+
+  def new
+    @comment = Comment.new
+  end
+
+  def show
+    comment = Comment.find_by(user: current_user, recipe: find_by_recipe_id)
+  end
+
+  def create
+    if logged_in?
+      comment = Comment.new(comment_params)
+      comment.recipe = find_by_recipe_id
+      comment.user = current_user
+      comment.save
+    else
+      redirect_to login_path, alert: "You must be logged in to comment"
+    end
+  end
+
+  def edit
+    @comment = find_by_id(Comment)
+  end
+
+  def update
+    comment = find_by_id(Comment)
+    comment.update(comment_params)
+    redirect_to recipe_path(comment.recipe), notice: "Your comment has been updated"
+  end
+
+  def destroy
+    id = params["id"].to_i
+    comment = Comment.find_by(id: id)
+    comment.delete
+    flash[:notice] = "Comment has been deleted"
+  end
+
+  private
+  
+  def comment_params
+     params.require(:comment).permit(:rating, :description)
+  end
+
 end
--- db/migrate/20180609143141_create_comments.rb
+++ db/migrate/20180609143141_create_comments.rb
@@ -1,7 +1,11 @@
 class CreateComments < ActiveRecord::Migration[5.2]
   def change
     create_table :comments do |t|
-
+      t.belongs_to :recipe, null: false
+      t.belongs_to :user, null: false
+      t.integer :rating, null: false
+      t.string :description
+      
       t.timestamps
     end
   end
