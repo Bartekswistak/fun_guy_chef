10c0d904f293ee64875169f37bc70150d1794d4e
github username <github email address>
2018-07-16 14:37:42 -0400

    rendering json with comments and serializers are working

--- app/assets/javascripts/recipes.js
+++ app/assets/javascripts/recipes.js
@@ -1,20 +1,9 @@
 $(function() {
   $("#new_comment").on("submit", function(e) {
 
-//    $.ajax({
-//      method: "GET",
-  //    url: this.href
-
-    //}).done(function(response){
-      //debugger
-      //$("div.comments").html(response)
-    //});
     const values = $(this).serialize()
 
     $.post(this.action, values).success(function(response) {
-//      debugger
-  //    $("div.comments").html(response)
-    //})
 
     $('div.comments').append(response)
     })
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -18,7 +18,8 @@ class CommentsController < ApplicationController
         else
           comment.save
           #redirect_to recipe_path(comment.recipe), notice: "Your comment has been added"
-          render json: comment
+          render json: comment.to_json(only: [:rating, :description],
+                                      include: [user: { only: [:name]}])
       end
     else
       redirect_to login_path, alert: "You must be logged in to comment"
