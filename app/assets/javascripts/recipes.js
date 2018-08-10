e23ff130d08f266dbb04bacfc23168ad89f32d93
github username <github email address>
2018-08-10 16:12:33 -0400

    appended edit link upon creating a new comment

--- app/assets/javascripts/recipes.js
+++ app/assets/javascripts/recipes.js
@@ -6,19 +6,20 @@ $(function() {
 
     $.post(this.action, values).success(function(response) {
 
+
       let heading = document.createElement("h3")
       let description = document.createElement("p")
       let edit_link = document.createElement("a")
-      let edit_url = `/recipes/${response.recipe_id}/comments/${response.id}/edit`
 
       heading.innerHTML = `${response.user.name} gives ${response.rating} out of 5 stars!`
       description.innerHTML = response.description
-      edit_link.innerHTML = "<a href=`${edit_url}`/>"
+      edit_link.setAttribute('href', `/recipes/${response.recipe_id}/comments/${response.id}/edit`)
+      edit_link.innerHTML = "Edit"
 
     $('div.comments').append(heading)
     $('div.comments').append(description)
     $('div.comments').append(edit_link)
-
+debugger
     })
 
     $('form#new_comment')[0].reset()
