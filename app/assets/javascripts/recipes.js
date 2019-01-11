eaa3d0c6a700875afba54c7825925f85a350b410
BartekSwistak <bartekswistak24@gmail.com>
2019-01-11 14:27:53 -0800

    added proper format for new ingredient

--- app/assets/javascripts/recipes.js
+++ app/assets/javascripts/recipes.js
@@ -9,12 +9,12 @@ $(document).ready(function() {
 	var wrapper   		= $(".ingredient_form"); 
 	var add_button      = $(".add_field_button"); //Add button ID
 	
-	var x = 1; //initlal text box count
+	var x = 1; //initial text box count
 	$(add_button).click(function(e){ //on add input button click
 		e.preventDefault();
 		if(x < max_fields){ //max input box allowed
 			x++; //text box increment
-      $(wrapper).append('<div><input type="text" name="mytext[]"/><a href="#" class="remove_field">Remove</a></div>');
+      $(wrapper).append('<div>Select Ingredient:<input type="text" name="recipe[recipe_ingredients_attributes][][ingredient_id]"/>Or Add New Ingredient:<input type="text" name="recipe[recipe_ingredients_attributes][][ingredient][name]"/>Amount:<input type="text" name="recipe[recipe_ingredients_attributes][][quantity]"/><a href="#" class="remove_field">Remove</a></div>');
 		}
 	});
 	
