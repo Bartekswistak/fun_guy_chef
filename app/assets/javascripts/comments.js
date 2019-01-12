c1e4d6d274d03ca7b8b3ea8ebac26302ef81f29f
BartekSwistak <bartekswistak24@gmail.com>
2019-01-12 11:26:13 -0800

    commented out old js code for creating comments and working to streamline process

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -7,28 +7,28 @@ $(function() {
       $.post(this.action, values).success(function(response) {
   
   
-        let heading = document.createElement("h3")
-        let description = document.createElement("p")
-        let edit_link = document.createElement("a")
-        let delete_link = document.createElement("a")
+        // let heading = document.createElement("h3")
+        // let description = document.createElement("p")
+        // let edit_link = document.createElement("a")
+        // let delete_link = document.createElement("a")
   
-        heading.innerHTML = `${response.user.name} gives ${response.rating} out of 5 stars!`
-        description.innerHTML = response.description
+        // heading.innerHTML = `${response.user.name} gives ${response.rating} out of 5 stars!`
+        // description.innerHTML = response.description
   
-        edit_link.setAttribute('href', `/recipes/${response.recipe_id}/comments/${response.id}/edit`)
-        edit_link.className = "ecomment"
-        edit_link.innerHTML = "Edit"
+        // edit_link.setAttribute('href', `/recipes/${response.recipe_id}/comments/${response.id}/edit`)
+        // edit_link.className = "ecomment"
+        // edit_link.innerHTML = "Edit"
   
-        delete_link.setAttribute('href', `/comments/${response.id}`)
-        delete_link.setAttribute('data-method', 'delete')
-        delete_link.className = "dcomment"
-        delete_link.innerHTML = "Delete"
+        // delete_link.setAttribute('href', `/comments/${response.id}`)
+        // delete_link.setAttribute('data-method', 'delete')
+        // delete_link.className = "dcomment"
+        // delete_link.innerHTML = "Delete"
   
-      $('div.comments').append(heading)
-      $('div.comments').append(description)
-      $('div.comments').append(edit_link, " ", delete_link)
+      // $('div.comments').append(heading)
+      // $('div.comments').append(description)
+      // $('div.comments').append(edit_link, " ", delete_link)
   
-      $('div.comments').wrap('<div class= "new_comment_container"/>').attr('id', `${response.id}`)
+      // $('div.comments').append('<div class= "new_comment_container"/>').attr('id', `${response.id}`)
   
       })
   
