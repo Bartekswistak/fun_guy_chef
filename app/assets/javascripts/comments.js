744e9d58386be507511bb5da53ed61f9fd96b32f
github username <github email address>
2019-03-13 16:31:23 -0400

    updated security issues in gemfile

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,7 +1,7 @@
 $(function createComment() {
   $("#new_comment").on("submit", function(e) {
     e.preventDefault();
-     
+
     const values = {
       description: $('#comment_description').val(),
       rating: $('#comment_rating').val()
@@ -9,30 +9,30 @@ $(function createComment() {
 
     const newComment = new Comment(values);
     newComment.commentConfirm();
-    
+
   });
 });
 
-Comment.prototype.commentConfirm = function(e) {    
+Comment.prototype.commentConfirm = function(e) {
   let doIt = confirm(`You are about to comment: "${this.description}" and give a rating of: ${this.rating} stars`);
   if(doIt == true) {
 
     let params = {
       'comment[description]': this.description,
       'comment[rating]': this.rating
-    };  
-    
+    };
+
   $.post(this.action, params).success(function(response) {
-    
+
     $('div.comments_container').append('<div class="new_comment_' + `${response.id}` + '"> </div>')
 
       $('div.new_comment_'+ `${response.id}`).append('<h3 class="cheading">' + response.user.name + ' gives ' + response.rating + ' out of 5 stars! </h3>')
       $('div.new_comment_'+ `${response.id}`).append('<p class="cdescription">' + response.description + '</p>')
       $('div.new_comment_'+ `${response.id}`).append('<a class="ecomment" href="/recipes/' + `${response.recipe_id}` + '/comments/' + `${response.id}` + '/edit">Edit</a>' + " ")
       $('div.new_comment_'+ `${response.id}`).append('<a class="dcomment" rel="nofollow" data-method="delete" href="/comments/' + `${response.id}` + '">Delete</a>')
-      
+
       $('form#new_comment')[0].reset();
-    });  
+    });
   };
 };
 
@@ -41,11 +41,11 @@ function Comment(comment) {
   this.rating = comment.rating;
   this.user = comment.user;
 }
-  
+
   $(function deleteComment() {
     $('body').on("click",'a.dcomment', function(e){
       e.preventDefault();
-      
+
       var r = confirm("Delete this comment?");
         if (r == true) {
           $(this).parent().hide("slow");
@@ -55,30 +55,30 @@ function Comment(comment) {
           }
       });
   });
-  
- 
+
+
   $(function editComment(){
     $('body').on("click",'a.ecomment', function(e){
-      e.preventDefault();        
+      e.preventDefault();
       $.get(this.href).success(function(data){
         var editForm = $(data).find('.edit_comment').html();
         var id = $(data).find('.edit_comment')[0].id.match(/\d+/)[0]
-        
+
           $('div.new_comment_' + id).hide();
-          $('div.comments_container').prepend('<div class="edit_comment_form"><h3> Edit your Comment: </h3>' + editForm + "<button class ='cancel_edit'>Cancel</button></div>")
-        
-          
+          $('div.comments_container').append('<div class="edit_comment_form"><h3> Edit your Comment: </h3>' + editForm + "<button class ='cancel_edit'>Cancel</button></div>")
+
+
        });
   });
 });
-      //Edit form with values is successfully displayed, now work on the actual updating 
+      //Edit form with values is successfully displayed, now work on the actual updating
 
   $(function canclEdit() {
     $('body').on("click", 'button.cancel_edit', function(){
       var id = $('.ecomment')[0].href.split("/")[6]
         $('div.new_comment_' + id).show();
         $('div.edit_comment_form').hide();
-              
+
     })
   })
 
@@ -86,10 +86,10 @@ function Comment(comment) {
     $('body').on("submit",'button#submit_comment'[0], function(e){
       e.preventDefault();
         alert('okay i got you')
-       
+
     })
   })
-  
+
 
   var updateComments = (data) => {
     $.ajax({
@@ -116,7 +116,3 @@ $(function() {
         })
     })
 });
-
-
-
-
