464c6c73d01bd81817839a51458a9ebf7dbf3d67
bartekswistak <bartekswistak24@gmail.com>
2019-04-14 12:20:22 -0700

    able to navigate all recipe links without page reload

--- app/assets/javascripts/recipes.js
+++ app/assets/javascripts/recipes.js
@@ -5,19 +5,19 @@ $(function displayRecipeInfo(){
 });
 
 $(function displayUserRecipeInfo(){
-	$('a#user_recipe').mouseenter(function(){
+	$('body').on('mouseenter', 'a#user_recipe', function(){
 		$(this).next('p.recipe_info').slideDown(2000);
 	})
 });
 
 $(function showUsersRecipes() {
-	$('a#users_recipes').click(function(e){
+	$('body').on("click",'a#users_recipes', function(e){
 		e.preventDefault();
 	
 		let thisUrl = this.href
 		
 	$.get(thisUrl).success(function(data){
-		let myRecipes = $(data).find('div.container').html();
+		let myRecipes = $(data).find('div.container');
 			$('div.container').replaceWith(myRecipes)
 			
 			window.history.pushState('obj', 'PageTitle', thisUrl);
@@ -27,7 +27,23 @@ $(function showUsersRecipes() {
 });
 
 $(function showRecipe() {
-	$('a#recipe_link').click(function(e){
+	$('body').on("click",'a#recipe_link', function(e){
+		e.preventDefault();
+	
+		let thisUrl = this.href
+		
+	$.get(thisUrl).success(function(data){
+		let thisRecipe = $(data).find('div.container')
+			$('div.container').replaceWith(thisRecipe)
+			
+			window.history.pushState('obj', 'PageTitle', thisUrl);
+   				return false;
+		});
+	});
+});
+
+$(function showRecipeFromUsers() {
+	$('body').on("click", 'a#user_recipe', function(e){
 		e.preventDefault();
 	
 		let thisUrl = this.href
@@ -46,10 +62,10 @@ $(function editRecipe(){
 	$('body').on("click",'a#edit_recipe', function(e){
 		e.preventDefault();
 
-	var thisRecipe = $('div.container')[0]
+	var thisRecipe = $('div.container')
 	
 	$.get(this.href).success(function(data){
-		var editForm = $(data).find('div.container').html();
+		var editForm = $(data).find('div.container');
 		$(thisRecipe).replaceWith(editForm)
 			$('body').on("click", 'button.cancel_edit_recipe', function(){
 				$('div.edit_recipe_form').replaceWith(thisRecipe);
@@ -57,6 +73,39 @@ $(function editRecipe(){
 		});
 	});
 });
+
+$(function showAllRecipes() {
+	$('body').on("click", 'a#all_recipes', function(e){
+		e.preventDefault();
+	
+		let thisUrl = this.href
+		
+	$.get(thisUrl).success(function(data){
+		let thisRecipe = $(data).find('div.container')
+			$('div.container').replaceWith(thisRecipe)
+			
+			window.history.pushState('obj', 'PageTitle', thisUrl);
+   				return false;
+		});
+	});
+});
+
+$(function showFastestRecipes() {
+	$('body').on("click", 'a#fastest_recipes', function(e){
+		e.preventDefault();
+	
+		let thisUrl = this.href
+		
+	$.get(thisUrl).success(function(data){
+		let thisRecipe = $(data).find('div.container')
+			$('div.container').replaceWith(thisRecipe)
+			
+			window.history.pushState('obj', 'PageTitle', thisUrl);
+   				return false;
+		});
+	});
+});
+
 
 $(function addMoreIngredients() {
 	var max_fields = 10;
@@ -126,21 +175,6 @@ $(function addMoreIngredients() {
 // 	});
 // });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // $(function(){
 // 	$('#submit_recipe').click(function(e){
 // 		e.preventDefault();
@@ -164,6 +198,7 @@ $(function addMoreIngredients() {
 		
 // 	})
 // 	})
+
 // })
 
 // function Recipe(recipe) {
